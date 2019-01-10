221cf4207ce6e0cfa61ed3183e90063c4df4ca39
Jared Harbison <jared.harbison@gmail.com>
2019-01-10 00:28:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,9 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {}
+  monopoly = {
+    :railroads
+  }
   monopoly[:railroads] = {:pieces => 4, :names => {}}
   monopoly
 end
